addebf794b1615129aa7b7a98981a1fd6c8014f0
Acan <acanthuridae@foxmail.com>
2023-08-06 22:55:52 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,60 @@
 import React from "react"
+import axios from "axios"
+import config from "./config.json"
+import "./App.css"
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            quote: "",
+            author: "",
+            category: ""
+        }
+    }
+
+    componentDidMount() {
+        this.getQuote = this.getQuote.bind(this)
+        this.getQuote()
+    }
+
     render() {
         return (
-            <div>
-                test
+            <div id="sub-root" className="container d-flex align-items-center justify-content-center">
+                <blockquote className="pb-5 fade-in">
+                    <p>
+                        {
+                            this.state.quote
+                        }
+                    </p>
+                    <br />
+                    <cite>
+                        {
+                            this.state.author
+                        }
+                    </cite>
+                </blockquote>
             </div>
         )
     }
+
+    getQuote = () => {
+        axios.get("https://api.api-ninjas.com/v1/quotes?category=freedom", {
+            headers: {
+                "X-Api-Key": config["api key"]
+            }
+        }).then(res => {
+            this.setState({
+                quote: res.data[0].quote,
+                author: res.data[0].author,
+                category: res.data[0].category
+            })
+        }).catch(err => {
+            console.log("ERROR", err)
+        })
+    }
+
 }
 
 export default App
